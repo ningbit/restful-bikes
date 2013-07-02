d4bcfb2062868bcba40815d003aca005f5760c4f
Ning Yap <ning.yap@gmail.com>
2013-07-02 17:00:23 -0400

    Added maps

--- app.rb
+++ app.rb
@@ -15,20 +15,48 @@ DataMapper.auto_migrate!
 module Bikes
   class App < Sinatra::Base
     get '/' do
-      debugger
       "Hello world"
     end
 
+    # list stations
     get '/stations' do
-    end
-
-    get '/stations/:id' do
+      @stations = Station.all.sort_by { |e| e.city }
+      erb :index
     end
 
     get '/stations/new' do
+      @station = Station.new
+      erb :new
     end
 
-    get '/station/:id/edit' do
+    get '/stations/:id' do
+      @station = Station.get(params[:id])
+      erb :show
+    end
+
+    get '/station/edit/:id' do
+      @station = Station.get(params[:id])
+      erb :edit
+    end
+
+    #create new station
+    post '/stations' do
+      @station = Station.new(params[:station])
+      if @station.save
+        status 201
+      redirect '/rabbits/' + @station.id.to_s
+      else
+      status 400
+      erb :new
+      end
+    end
+
+    get '/stations/delete/:id' do
+      #delete confirmation
+    end
+
+    delete '/stations/:id' do
+      #delete
     end
 
   end
